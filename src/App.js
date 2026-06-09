136a88f6a4fbf55f58aed4feedb50b282e8ceb82
mariumrashad <mariumrashad9@gmail.com>
2026-06-09 22:23:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { useEffect, useState, createContext, useContext } from "react";
 import Header from "./components/Header/index"; 
 import Footer from "./components/Footer";
